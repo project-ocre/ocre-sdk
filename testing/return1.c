5940c19468f27b676765d34c67d469cfc9408869
Marco Casaroli <marco.casaroli@gmail.com>
2025-12-10 14:20:42 +0100

    feat: testing containers
    We will use those for automated testing.
    
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

---
+++ testing/return1.c
@@ -0,0 +1,15 @@
+/*
+ * @copyright Copyright © contributors to Project Ocre,
+ * which has been established as Project Ocre a Series of LF Projects, LLC
+
+ * SPDX-License-Identifier: Apache-2.0
+
+ */
+
+#include <stdio.h>
+
+// Application entry point
+int main()
+{
+    return 1;
+}
---
+++ testing/sleep_5_return_0.c
@@ -0,0 +1,17 @@
+/*
+ * @copyright Copyright © contributors to Project Ocre,
+ * which has been established as Project Ocre a Series of LF Projects, LLC
+
+ * SPDX-License-Identifier: Apache-2.0
+
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+// Application entry point
+int main()
+{
+    sleep(5);
+    return 0;
+}
