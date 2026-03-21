79e0b52053cf42e603dcf9b83ed7da7080e5c054
Marco Casaroli <marco.casaroli@gmail.com>
2026-03-21 16:12:32 +0100

    refactor: separate sysroot
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

--- generic/hello-world/main.c
+++ generic/hello-world/main.c
@@ -5,17 +5,17 @@
  * SPDX-License-Identifier: Apache-2.0
 
  */
- 
+
 #include <stdio.h>
 
 // Application entry point
 int main()
-{  
+{
     printf("\n\
      _      _________  ___   ______________  ______  ____  __ \n\
     | | /| / / __/ _ )/ _ | / __/ __/ __/  |/  / _ )/ /\\ \\/ / \n\
     | |/ |/ / _// _  / __ |_\\ \\_\\ \\/ _// /|_/ / _  / /__\\  /  \n\
     |__/|__/___/____/_/ |_/___/___/___/_/  /_/____/____//_/   \n\
-                                           powered by Ocre  ");
+                                           powered by Ocre  \n");
     return 0;
 }
--- scripts/wasm32-wasi-pthread.cmake
+++ scripts/wasm32-wasi-pthread.cmake
@@ -1,31 +1,18 @@
 # Cmake toolchain description file for the Makefile
 
 if(NOT WASI_SYSROOT)
-    set(WASI_SYSROOT "/opt/wasi-sysroot")
+    set(WASI_SYSROOT "${CMAKE_CURRENT_LIST_DIR}/../sysroot")
 endif()
 
 set(CMAKE_SYSTEM_NAME WASI)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR wasm32)
+
 set(triple wasm32-wasi-threads)
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread --sysroot=${WASI_SYSROOT}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread --sysroot=${WASI_SYSROOT}")
-# wasi-threads requires --import-memory.
-# wasi requires --export-memory.
-# (--export-memory is implicit unless --import-memory is given)
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--import-memory")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--export-memory")
 
-if(WIN32)
-	set(WASI_HOST_EXE_SUFFIX ".exe")
-else()
-	set(WASI_HOST_EXE_SUFFIX "")
-endif()
-
-# When building from source, WASI_SDK_PREFIX represents the generated directory
-if(NOT WASI_SDK_PREFIX)
-    set(WASI_SDK_PREFIX ${CMAKE_CURRENT_LIST_DIR}/../../)
-endif()
+set(CMAKE_C_FLAGS "-pthread --sysroot=${WASI_SYSROOT}")
+set(CMAKE_CXX_FLAGS "-pthread --sysroot=${WASI_SYSROOT}")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--import-memory -Wl,--export-memory -Wl,--strip-all -Wl,--allow-undefined -Wl,--max-memory=4194304")
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
